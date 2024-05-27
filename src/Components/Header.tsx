6d59f43f16865156377ec2be0ff5bc3abd11719e
jreitz2 <jreitz2@gmail.com>
2024-05-27 08:47:21 -0400

    add 3 filters and recipes on open to match native version

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { SetRecipesType, SetLoadingType } from "../types";
 
 type HeaderProps = {
@@ -7,9 +7,12 @@ type HeaderProps = {
 };
 
 const Header = ({ setRecipes, setLoading }: HeaderProps) => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("bacon, sugar, flour");
   const [isVegetarian, setIsVegetarian] = useState(false);
   const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isDairyFree, setIsDairyFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isKetogenic, setIsKetogenic] = useState(false);
   const [noResultsFound, setNoResultsFound] = useState(false);
 
   const handleSearchTermChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -24,21 +27,37 @@ const Header = ({ setRecipes, setLoading }: HeaderProps) => {
     setIsGlutenFree(!isGlutenFree);
   };
 
-  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleDairyChange = () => {
+    setIsDairyFree(!isDairyFree);
+  };
+
+  const handleVeganChange = () => {
+    setIsVegan(!isVegan);
+  };
+
+  const handleKetogenicChange = () => {
+    setIsKetogenic(!isKetogenic);
+  };
+
+  const handleSearch = async (e?: FormEvent<HTMLFormElement>) => {
+    if (e) {
+      e.preventDefault();
+    }
     setLoading(true);
     const noSpaceSearchTerm = searchTerm.replace(" ", "");
     const key = import.meta.env.VITE_API_KEY;
-    let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${noSpaceSearchTerm}&number=6&addRecipeInformation=true&addRecipeInstructions=true`;
-    if (isVegetarian && !isGlutenFree) {
-      url += "&diet=vegetarian";
+    let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${noSpaceSearchTerm}&number=20&addRecipeInformation=true&addRecipeInstructions=true`;
+    let diets = [];
+    if (isVegetarian) diets.push("vegetarian");
+    if (isGlutenFree) diets.push("gluten-free");
+    if (isDairyFree) diets.push("ovo-vegetarian");
+    if (isVegan) diets.push("vegan");
+    if (isKetogenic) diets.push("ketogenic");
+
+    if (diets.length > 0) {
+      url += `&diet=${diets.join(",")}`;
     }
-    if (isGlutenFree && !isVegetarian) {
-      url += "&diet=gluten-free";
-    }
-    if (isGlutenFree && isVegetarian) {
-      url += "&diet=vegetarian,gluten-free";
-    }
+
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -65,6 +84,10 @@ const Header = ({ setRecipes, setLoading }: HeaderProps) => {
       console.log(err);
     }
   };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
 
   return (
     <div className="header">
@@ -96,7 +119,33 @@ const Header = ({ setRecipes, setLoading }: HeaderProps) => {
           checked={isGlutenFree}
           onChange={handleGlutenChange}
         />
-        <label htmlFor="gluten-free"> Gluten-free</label>
+        <label htmlFor="gluten-free"> Gluten-free </label>
+        <input
+          type="checkbox"
+          id="dairy-free"
+          name="dairy-free"
+          checked={isDairyFree}
+          onChange={handleDairyChange}
+        />
+        <label htmlFor="vegan"> Dairy-free </label>
+
+        <input
+          type="checkbox"
+          id="vegan"
+          name="vegan"
+          checked={isVegan}
+          onChange={handleVeganChange}
+        />
+        <label htmlFor="vegan"> Vegan </label>
+
+        <input
+          type="checkbox"
+          id="ketogenic"
+          name="ketogenic"
+          checked={isKetogenic}
+          onChange={handleKetogenicChange}
+        />
+        <label htmlFor="ketogenic"> Ketogenic </label>
       </form>
       {noResultsFound && <p>No results found. Try less ingredients/filters.</p>}
     </div>
